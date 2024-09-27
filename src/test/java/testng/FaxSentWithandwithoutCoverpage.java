ae2a78f61170c2772c74c8ea947a6c50726cacb5
aahmadov <abbas@softlinx.com>
2024-09-27 17:32:53 -0400

    SSO credential changed

--- src/test/java/testng/FaxSentWithandwithoutCoverpage.java
+++ src/test/java/testng/FaxSentWithandwithoutCoverpage.java
@@ -40,8 +40,7 @@ public class FaxSentWithandwithoutCoverpage extends TestBase {
         loginPageObj.sendFaxButton.click();
         loginPageObj.faxNumber.click();
         loginPageObj.faxNumber.sendKeys(String.valueOf(data.get("FaxNumber")));
-//        Thread.sleep(1000 * 3);
-//        loginPageObj.uploadPage.click();
+
             File pagesSize = FileReader.getFileUsingPageSize(data.get("pageSize"), data.get("fileType"));
         // Wait until the form is visible
         WebDriverWait wait3 = new WebDriverWait(driver, Duration.ofSeconds(20));
@@ -56,25 +55,7 @@ public class FaxSentWithandwithoutCoverpage extends TestBase {
                 "var mockFile = { name: '" + fileToUpload.getName() + "', size: " + fileToUpload.length() + " };" +
                 "dropzone.emit('addedfile', mockFile);" +
                 "dropzone.emit('complete', mockFile);");
-//            Robot rb = new Robot();
-//            rb.delay(1000 * 2);
-//            //put the path to file in clipboard
-//            StringSelection Filepath = new StringSelection(pagesSize.toString());
-//            System.out.println("File Name: " + pagesSize.toString());
-//            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(Filepath, null);
-//            Thread.sleep(1000 * 3);
-//            rb.keyPress(KeyEvent.VK_ENTER);
-//            rb.keyRelease(KeyEvent.VK_ENTER);
-//            rb.delay(1000);
-//            rb.keyPress(KeyEvent.VK_CONTROL);
-//            rb.keyPress(KeyEvent.VK_V);
-//            rb.delay(1000);
-//            rb.keyRelease(KeyEvent.VK_CONTROL);
-//            rb.keyRelease(KeyEvent.VK_V);
-//            rb.delay(300);
-//            rb.keyPress(KeyEvent.VK_ENTER);
-//            rb.keyRelease(KeyEvent.VK_ENTER);
-//            rb.delay(1000 * 5);
+
             loginPageObj.ClickSendButton.click();
             loginPageObj.confirmationButton.click();
 
@@ -118,16 +99,12 @@ public class FaxSentWithandwithoutCoverpage extends TestBase {
                 "var mockFile = { name: '" + fileToUpload.getName() + "', size: " + fileToUpload.length() + " };" +
                 "dropzone.emit('addedfile', mockFile);" +
                 "dropzone.emit('complete', mockFile);");
-//        Robot rb = new Robot();
-//        rb.delay(1000 * 2);
-//        //put the path to file in clipboard
-//        StringSelection Filepath = new StringSelection(pagesSize.toString());
 
         loginPageObj.ClickSendButton.click();
         loginPageObj.confirmationButton.click();
 
     }
-    @Test(priority = 3,testName = "Send Fax from portal with coverPage ", groups = {"Regression84"})
+    @Test(priority = 3,testName = "Send Fax from portal withNo coverPage ", groups = {"Regression84"})
     public void FaxSentandRecvWithandwithoutCoverPage() throws InterruptedException, AWTException {
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Test case name: " + testName);
