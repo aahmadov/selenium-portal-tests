dc38ceff86d4fd1274d4849042dd884c63c27a44
aahmadov <abbas@softlinx.com>
2024-10-14 15:10:23 -0400

    new case resendFailedFax test

--- src/test/java/testng/Resend_Failed_Fax.java
+++ src/test/java/testng/Resend_Failed_Fax.java
@@ -37,99 +37,98 @@ public class Resend_Failed_Fax extends TestBase {
         System.out.println(response.asPrettyString());
         System.out.println("**" + (data.get("put_call_Url")));
         System.out.println("------------------------------------------------------------------------");
-        driver.get("http://10.250.1.84:80/");
-        Thread.sleep(1000 * 3);
-        /*This operation will maximize window*/
-        driver.manage().window().maximize();
-        LoginPageTestNG loginPageObj = new LoginPageTestNG(driver);
-        loginPageObj.UsernameTextBox.sendKeys(data.get("Username"));
-        loginPageObj.PasswordTextBox.sendKeys(data.get("password"));
-        loginPageObj.loginButton.click();
+//        driver.get("http://10.250.1.84:80/");
+//        Thread.sleep(1000 * 3);
+//        /*This operation will maximize window*/
+//        driver.manage().window().maximize();
+//        LoginPageTestNG loginPageObj = new LoginPageTestNG(driver);
+//        loginPageObj.UsernameTextBox.sendKeys(data.get("Username"));
+//        loginPageObj.PasswordTextBox.sendKeys(data.get("password"));
+//        loginPageObj.loginButton.click();
         Thread.sleep(1000 * 10);
         Response response3 = RestRequestUtils.reprocessScenario(data.get("post_call_start/Service"));
         System.out.println(response3);
-        Thread.sleep(1000 * 30);
-        loginPageObj.FaxingButton.click();
-        loginPageObj.sendFaxButton.click();
-        Thread.sleep(1000 * 3);
-        loginPageObj.faxNumber.click();
-        Thread.sleep(1000 * 3);
-        loginPageObj.faxNumber.sendKeys(String.valueOf(data.get("FaxNumber")));
-        Thread.sleep(1000 * 3);
-        loginPageObj.coverPage.click();
-        Thread.sleep(1000 * 3);
-        loginPageObj.ChoseCoverpage.click();
+//        Thread.sleep(1000 * 30);
+//        loginPageObj.FaxingButton.click();
+//        loginPageObj.sendFaxButton.click();
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.faxNumber.click();
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.faxNumber.sendKeys(String.valueOf(data.get("FaxNumber")));
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.coverPage.click();
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.ChoseCoverpage.click();
+//
+//        Thread.sleep(1000*3);
+//        loginPageObj.ClickSendButton.click();
+//        Thread.sleep(1000*3);
+//        loginPageObj.confirmationButton.click();
+//        Thread.sleep(1000 * 10);
+//        loginPageObj.AdministrationHeading.click();
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.AdministrationfilterFaxstatus.click();
+//        Thread.sleep(1000 * 3);
+//        loginPageObj.OutboundRadiobox.click();
+//
+//        JavascriptExecutor js1 = (JavascriptExecutor) driver;
+//        // Scroll down the page by pixel (e.g., 500 pixels)
+//        js1.executeScript("window.scrollBy(100, 3000)");
+//        // Define the timeout duration in seconds
+//        int timeoutInSeconds = 180; // Adjust the timeout as needed
+//
+//        // Loop until the info button is visible or timeout occurs
+//        long startTime = System.currentTimeMillis();
+//        boolean InfoButton = false;
+//
+//        while (!InfoButton && (System.currentTimeMillis() - startTime) < timeoutInSeconds * 1000) {
+//            //Click the search button
+//            WebElement clickSearchBtn = driver.findElement(By.xpath("//a[@id='SearchForFaxes']"));
+//            clickSearchBtn.click();
+//
+//            // Wait for info btn to be visible
+//            try {
+//                WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(20)); // Wait up to 20 seconds for the info btn to appear
+//                wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='outboundFaxes']/tbody/tr[1]//button[5]")));
+//                InfoButton = true; // Exit the loop if Info btn is found
+//            } catch (org.openqa.selenium.TimeoutException e) {
+//                // info btn is not found yet, continue the loop
+//            }
+//        }
+//        // Click on info btn if found
+//        if (InfoButton) {
+//            WebElement infoBtn = driver.findElement(By.xpath("//table[@id='outboundFaxes']/tbody/tr[1]//button[5]"));
+//            infoBtn.click();
+//        } else {
+//            System.out.println("Timeout: Info button not found within " + timeoutInSeconds + " seconds.");
+//        }
+//        Thread.sleep(1000 * 5);
+//        loginPageObj.closeInfoBtn.click();
+//        Thread.sleep(1000 * 3);
+//        JavascriptExecutor scrollPage1 = (JavascriptExecutor) driver;
+//        scrollPage1.executeScript("window.scrollBy(100,3000)");
+//
+//        Thread.sleep(1000 * 5);
+//        loginPageObj.advancedActionBTN.click();
+//        Thread.sleep(1000 * 2);
+//        loginPageObj.ReprocessCoverPandDocConversion.click();
+//        WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(20));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ResendFaxSave']"))) ;
+//        WebElement clickSubmitBtn = driver.findElement(By.xpath("//*[@id='ResendFaxSave']"));
+//        clickSubmitBtn.click();
+////        loginPageObj.submitResendBTN.click();
+//        Thread.sleep(1000 * 3);
+//        WebDriverWait wait2 =new WebDriverWait(driver,Duration.ofSeconds(20));
+//        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='modal-footer']//button[contains(@class, 'btn btn-orange btn-sm dialogBtn') and contains(text(), 'OK')]"))) ;
+//        WebElement clickSubmitConfirmationBtn = driver.findElement(By.xpath("//div[@class='modal-footer']//button[contains(@class, 'btn btn-orange btn-sm dialogBtn') and contains(text(), 'OK')]"));
+//        clickSubmitConfirmationBtn.click();
+////        loginPageObj.confirmationsubmitResendBTN.click();
+//        Thread.sleep(1000 * 3);
+//        WebElement clickSearchBtn = driver.findElement(By.xpath("//a[@id='SearchForFaxes']"));
+//        clickSearchBtn.click();
+//        JavascriptExecutor scrollPage2 = (JavascriptExecutor) driver;
+//        scrollPage1.executeScript("window.scrollBy(100,3000)");
+//        Thread.sleep(1000 * 10);
 
-        Thread.sleep(1000*3);
-        loginPageObj.ClickSendButton.click();
-        Thread.sleep(1000*3);
-        loginPageObj.confirmationButton.click();
-        Thread.sleep(1000 * 10);
-        loginPageObj.AdministrationHeading.click();
-        Thread.sleep(1000 * 3);
-        loginPageObj.AdministrationfilterFaxstatus.click();
-        Thread.sleep(1000 * 3);
-        loginPageObj.OutboundRadiobox.click();
-
-        JavascriptExecutor js1 = (JavascriptExecutor) driver;
-        // Scroll down the page by pixel (e.g., 500 pixels)
-        js1.executeScript("window.scrollBy(100, 3000)");
-        // Define the timeout duration in seconds
-        int timeoutInSeconds = 180; // Adjust the timeout as needed
-
-        // Loop until the info button is visible or timeout occurs
-        long startTime = System.currentTimeMillis();
-        boolean InfoButton = false;
-
-        while (!InfoButton && (System.currentTimeMillis() - startTime) < timeoutInSeconds * 1000) {
-            //Click the search button
-            WebElement clickSearchBtn = driver.findElement(By.xpath("//a[@id='SearchForFaxes']"));
-            clickSearchBtn.click();
-
-            // Wait for info btn to be visible
-            try {
-                WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(20)); // Wait up to 20 seconds for the info btn to appear
-                wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='outboundFaxes']/tbody/tr[1]//button[5]")));
-                InfoButton = true; // Exit the loop if Info btn is found
-            } catch (org.openqa.selenium.TimeoutException e) {
-                // info btn is not found yet, continue the loop
-            }
-        }
-        // Click on info btn if found
-        if (InfoButton) {
-            WebElement infoBtn = driver.findElement(By.xpath("//table[@id='outboundFaxes']/tbody/tr[1]//button[5]"));
-            infoBtn.click();
-        } else {
-            System.out.println("Timeout: Info button not found within " + timeoutInSeconds + " seconds.");
-        }
-        Thread.sleep(1000 * 5);
-        loginPageObj.closeInfoBtn.click();
-        Thread.sleep(1000 * 3);
-        JavascriptExecutor scrollPage1 = (JavascriptExecutor) driver;
-        scrollPage1.executeScript("window.scrollBy(100,3000)");
-        Thread.sleep(1000 * 2);
-        Response response4 = RestRequestUtils.putScenario84Pportal(data.get("put_call_Url2"));
-        System.out.println(response4);
-        Thread.sleep(1000 * 5);
-        loginPageObj.advancedActionBTN.click();
-        Thread.sleep(1000 * 2);
-        loginPageObj.ReprocessCoverPandDocConversion.click();
-        WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ResendFaxSave']"))) ;
-        WebElement clickSubmitBtn = driver.findElement(By.xpath("//*[@id='ResendFaxSave']"));
-        clickSubmitBtn.click();
-//        loginPageObj.submitResendBTN.click();
-        Thread.sleep(1000 * 3);
-        WebDriverWait wait2 =new WebDriverWait(driver,Duration.ofSeconds(20));
-        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='modal-footer']//button[contains(@class, 'btn btn-orange btn-sm dialogBtn') and contains(text(), 'OK')]"))) ;
-        WebElement clickSubmitConfirmationBtn = driver.findElement(By.xpath("//div[@class='modal-footer']//button[contains(@class, 'btn btn-orange btn-sm dialogBtn') and contains(text(), 'OK')]"));
-        clickSubmitConfirmationBtn.click();
-//        loginPageObj.confirmationsubmitResendBTN.click();
-        Thread.sleep(1000 * 3);
-        WebElement clickSearchBtn = driver.findElement(By.xpath("//a[@id='SearchForFaxes']"));
-        clickSearchBtn.click();
-        JavascriptExecutor scrollPage2 = (JavascriptExecutor) driver;
-        scrollPage1.executeScript("window.scrollBy(100,3000)");
-        Thread.sleep(1000 * 10);
     }
 }
