1925fa51fce16e818267d9b70497bf8b5935c60c
aahmadov <abbas@softlinx.com>
2024-10-14 12:01:46 -0400

    new case resendFailedFax was added to regression

--- src/test/java/testng/ReprocessDocConversion.java
+++ src/test/java/testng/ReprocessDocConversion.java
@@ -24,7 +24,7 @@ import static org.testng.Assert.assertEquals;
 public class ReprocessDocConversion extends TestBase {
 
 
-    @Test(priority = 1, testName = "Portal:ReprocessDOCConversion ", groups = {"Regression84"})
+    @Test(priority = 1, testName = "Portal:ReprocessDOCConversion ", groups = {"Regression84test"})
     public void ReprocessDocConversionPortal() throws InterruptedException, AWTException, SQLException {
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         System.out.println("Test case name: " + testName);
